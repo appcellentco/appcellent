b32958bb2e8a3e40e45557882c7a82ae3a83a40d
batu <batudemirbas@icloud.com>
2026-05-21 17:55:32 +0300

    coloringpage add

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ cp -r protractor public/protractor
 cp -r tcgvault public/tcgvault
 cp -r volumebooster public/volumebooster
 cp -r limify public/limify
+cp -r coloringpage public/coloringpage
 cp -r styles public/styles
 cp -r scripts public/scripts
-for dir in public/main public/tcgvault public/studfinder public/volumebooster public/protractor public/limify; do
+for dir in public/main public/tcgvault public/studfinder public/volumebooster public/protractor public/limify public/coloringpage; do
   [ -f "$dir/index.html" ] && mv "$dir/index.html" "$dir/home.html"
 done
